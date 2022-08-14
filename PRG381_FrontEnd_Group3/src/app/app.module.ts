9aee9993636ffeadf12ce6810af36c45deb7f576
DeRaven <stoltz278@gmail.com>
2022-08-14 19:40:16 +0200

    added booking browser page

--- PRG381_FrontEnd_Group3/src/app/app.module.ts
+++ PRG381_FrontEnd_Group3/src/app/app.module.ts
@@ -6,18 +6,69 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookingBrowserService } from './services/booking-browser/booking-browser.service';
 import { BookingbrowserMockService } from './mock/mock-booking-browser/bookingbrowser-mock.service';
+import { BookingBrowserComponent } from './pages/booking-browser/booking-browser.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatTableModule } from "@angular/material/table";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatDialogContent, MatDialogModule } from "@angular/material/dialog";
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips';
+import {MatBadgeModule} from '@angular/material/badge'
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingBrowserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatBadgeModule,
+    ReactiveFormsModule,
+    MatDividerModule,
+    MatTooltipModule
   ],
-  providers: [],
+  providers: [
+    {provide: BookingBrowserService,useClass: BookingbrowserMockService} 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
