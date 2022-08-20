a1169f63fed55f6774dfdf1ac190ee67934b122c
DeRaven <stoltz278@gmail.com>
2022-08-20 18:02:55 +0200

    Created admin management services

--- PRG381_FrontEnd_Group3/src/app/services/admin-service/admin.service.ts
+++ PRG381_FrontEnd_Group3/src/app/services/admin-service/admin.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IDetailedAdmin } from 'src/app/models/admin-interface';
+import { environment } from 'src/environments/environment';
+
+//request Models for admin service
+export interface IDetailedAdminRequest{
+  adminID: string
+}
+
+export interface IUpdateAdminRequest{
+  admins: IDetailedAdmin[]
+}
+
+export interface IDeleteAdminRequest{
+  adminID: string
+}
+
+export interface IAdminManagementService{
+  getAdmin(request:IDetailedAdminRequest):Observable<IDetailedAdmin>
+  updateAdmin(request:IUpdateAdminRequest):Observable<Object>
+  deleteAdmin(request:IDeleteAdminRequest):Observable<Object>
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminService {
+export class AdminService implements IAdminManagementService{
+  api_url=environment.api_url+"/admin"
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getAdmin(request: IDetailedAdminRequest): Observable<IDetailedAdmin> {
+    let params= new HttpParams
+    params.set("adminID",request.adminID)
+      return this.http.get<IDetailedAdmin>(this.api_url+"/",{params:params});
+  }
+
+  updateAdmin(request: IUpdateAdminRequest): Observable<Object> {
+      return this.http.patch<Object>(this.api_url,request)
+  }
+    
+  deleteAdmin(request: IDeleteAdminRequest): Observable<Object> {
+      return this.http.delete<Object>(this.api_url,{body:request})
+  }
+
 }
