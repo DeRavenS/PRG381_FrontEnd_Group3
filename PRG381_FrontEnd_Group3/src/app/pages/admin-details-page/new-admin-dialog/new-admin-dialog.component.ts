e196291c08dda48d4147306395fcc6b85b94d15f
DeRaven <stoltz278@gmail.com>
2022-08-22 19:21:56 +0200

    added admin dialog component

---
+++ PRG381_FrontEnd_Group3/src/app/pages/admin-details-page/new-admin-dialog/new-admin-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-admin-dialog',
+  templateUrl: './new-admin-dialog.component.html',
+  styleUrls: ['./new-admin-dialog.component.css']
+})
+export class NewAdminDialogComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
