8ab21435f70f2f1f6e2fdc7968da1940b4f77864
DeRaven <stoltz278@gmail.com>
2022-08-28 18:33:44 +0200

    Connected backend routes

--- PRG381_FrontEnd_Group3/src/app/pages/login/login.component.ts
+++ PRG381_FrontEnd_Group3/src/app/pages/login/login.component.ts
@@ -1,5 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { CurrentUser } from 'src/app/models/currentUser';
+import { ILoginRequest, LoginRegisterManagementService } from 'src/app/services/login-register/login-register.service';
+import { IResetPasswordRequest, ResetPasswordService } from 'src/app/services/reset-password/reset-password.service';
 import { ResetPasswordDialogComponent } from './reset-password-dialog/reset-password-dialog.component';
 
 @Component({
@@ -10,10 +15,17 @@ import { ResetPasswordDialogComponent } from './reset-password-dialog/reset-pass
 export class LoginComponent implements OnInit {
   hide = true;
 
-  constructor(public dialog: MatDialog) { }
+  loginRequest :ILoginRequest={
+    password:"",
+    email:""
+  }
+  constructor(public dialog: MatDialog,private loginService:LoginRegisterManagementService,private resetPasswordService: ResetPasswordService, private router:Router) { }
+
 
   ngOnInit(): void {
   }
+
+  
 
   openDialogReset(){
     let dialogRef = this.dialog.open(ResetPasswordDialogComponent, {});
@@ -24,5 +36,31 @@ export class LoginComponent implements OnInit {
       }
     })
   }
+
+  login(){
+    if (this.loginRequest.email && this.loginRequest.email!=""){
+      this.loginService.loginUser(this.loginRequest).subscribe(val=>{
+        if (val){
+          CurrentUser._admin=val.admin;
+          CurrentUser.id=val.id;
+
+          console.log(val)
+          if (CurrentUser._admin==true) {
+            this.router.navigate(['/students']);
+          }
+          else {
+            
+            this.router.navigate(['/students/details'],{queryParams:{studentID:CurrentUser.id}});
+          }
+        }
+        else alert("Invalid Login")
+
+      },error=>{
+        alert("Login Failed")
+      })
+    }
+  }
+
+  
 
 }
